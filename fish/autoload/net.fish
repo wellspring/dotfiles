c16299b069c0419ca087285fedea32797d30fdcf
William Hubault <william.hubault@gmail.com>
2015-03-06 14:12:28 +0100

    (fish) Add gateway+resolve network functions + description for netip/lanip/lanprefix

--- fish/autoload/net.fish
+++ fish/autoload/net.fish
@@ -7,13 +7,20 @@ alias wget-fast='axel -a'
 alias wifi='RUN_AS_ROOT wifi-menu'
 
 # Other aliases
-function netip
+function netip -d "Get the Internet IP address (ipv4; through icanhazip.com)"
     curl icanhazip.com
 end
-function lanip
+function lanip -d "Get the LAN IP address (ipv4)"
     ip addr show dev (ip route | awk "/^default/ {print \$5}") | awk -F"[ /]*" "/scope global/ { print \$3 }"
 end
-function lanprefix
+function lanprefix -d "Get the LAN prefix (e.g. 24)"
     ip addr show dev (ip route | awk "/^default/ {print \$5}") | awk -F"[ /]*" "/scope global/ { print \$4 }"
+end
+function gateway -d "Get the default gateway (e.g. 192.168.1.1)"
+    ip route | awk '/default/ {print $3}'
+end
+function resolve -d "Return all IP Adresses associated with a hostname (using nmap)"
+   # resolve <host> [host [host [...]]]
+   sudo nmap -Pn -sL -R $argv | grep --color=never -E -o -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -e 'scan report for (\S+)' | sed 's/scan report for \(.*\)/\x1b[41m[\1]\x1b[00m/'
 end
 
