b42388e319fe3bbc1a478f80f07511e2ddf614a5
William Hubault <william.hubault@gmail.com>
2015-04-01 17:20:16 +0200

    (fish) clean the main fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,40 +1,30 @@
 # ********************************************************************* #
 #                            * config.fish *                            #
 #                              ~~~~~~~~~~~                              #
-#   Author         : wellspring <wellspring.fr A T gmail.com>           #
-#   Description    : BASHrc/ZSHrc mix of configurations (dotfiles...)   #
-#   Last modified  : 27/02/2015                                         #
-#   Version        : 1.8.2                                              #
+#   Author         : William Hubault                                    #
+#   Description    : fish shell configuration                           #
+#   Last modified  : 17/03/2015                                         #
+#   Version        : 1.9.0                                              #
 # ********************************************************************* #
 
 # Configure the environment variables
-set -x TERMINAL st
-set -x BROWSER chromium
-set -x SUDO_EDITOR vim
-set -x SVN_EDITOR vim
-set -x EDITOR vim
-set -x VISUAL vim
-set -x PAGER less
+set -x EDITOR "vim -X --noplugin"
+set -x SVN_EDITOR "$EDITOR"
+set -x SUDO_EDITOR "$EDITOR"
+set -x VISUAL "vim"
+set -x PAGER "less"
+set -x BROWSER "chromium"
+set -x TERMINAL "st"
 
-#set -x CDPATH ...
-#set -x PATH ...
 set -x SBT_OPTS "-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M"
 set -x VMWARE_USE_SHIPPED_GTK yes
 set -x RAILS_ENV development
 
+# Configure paths
+#set -x CDPATH ...
+#set -x PATH ...
 
-# Configure fish to use oh-my-fish themes/plugins...
-#set fish_plugins archlinux autojump bundler extract gi localhost node rake rvm
+# Configure fish paths
 set fish_complete_path ~/.config/fish/completions /etc/fish/completions /usr/share/fish/completions
 set fish_function_path ~/.config/fish/prompt ~/.config/fish/functions /etc/fish/functions /usr/share/fish/functions
-
-# Define an exit handler (also possible to use trap)
-function on_exit --on-process %self
-    echo fish is now exiting
-end
-
-# Load each auto-load files
-for file in ~/.config/fish/autoload/**
-    . $file
-end
 
