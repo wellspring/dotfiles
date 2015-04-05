3fdd9f8ad032dd39ec9095e4cfb63a4e8c2bfe52
William Hubault <william.hubault@gmail.com>
2015-04-05 19:44:13 +0200

    (fish) improve function pathadd to check if directory exist before adding it, like on zsh

--- fish/functions/pathadd.fish
+++ fish/functions/pathadd.fish
@@ -1,3 +1,9 @@
 function pathadd --description 'Add a path to the $PATH variable'
-	set -U fish_user_paths $fish_user_paths $argv;
+	for p in $argv
+    if test -d $p
+      set -U fish_user_paths $fish_user_paths $argv;
+    else
+      echo "Error: not adding path \"$p\" since the directory doesn't exists. Is it a mistake?"
+    end
+  end
 end
