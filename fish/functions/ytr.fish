0452cd521838747a0e31e784d80f48dc5678e271
William Hubault <william.hubault@gmail.com>
2015-04-08 16:07:00 +0200

    (fish) improve ytr: display which youtube video url is played, with which title.

--- fish/functions/ytr.fish
+++ fish/functions/ytr.fish
@@ -1,4 +1,10 @@
 function ytr --description 'Play a youtuberss video with vlc (Usage: ytr <#line>)'
 	set -l cache_file "/tmp/youtube_rss.cache"
-  line $argv[1] "$cache_file" | cut -d^ -f1 | xargs vlc 2>/dev/null
+  set -l title (line $argv[1] "$cache_file" | cut -d^ -f2 | sed 's/ *$//')
+  set -l url (line $argv[1] "$cache_file" | cut -d^ -f1)
+
+  echo Playing (echo "$title" | colorize 198) ...
+  echo " -> youtube url: $url"
+
+  vlc "$url" 2>/dev/null &
 end
