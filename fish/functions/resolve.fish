24b3dc5dee16aa701c1a92947d115c974b192b39
William Hubault <william.hubault@gmail.com>
2015-05-08 14:02:50 +0200

    (fish) make resolve function use RUN_AS_ROOT instead

--- fish/functions/resolve.fish
+++ fish/functions/resolve.fish
@@ -1,4 +1,3 @@
 function resolve --description 'Return all IP Adresses associated with a hostname (using nmap)'
-	
-   sudo nmap -Pn -sL -R $argv | grep --color=never -E -o -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -e 'scan report for (\S+)' | sed 's/scan report for \(.*\)/\x1b[41m[\1]\x1b[00m/'
+	RUN_AS_ROOT nmap -Pn -sL -R $argv | grep --color=never -E -o -e '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -e 'scan report for (\S+)' | sed 's/scan report for \(.*\)/\x1b[41m[\1]\x1b[00m/'
 end
