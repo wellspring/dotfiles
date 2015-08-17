192e44966014892c4474e1c0e7e777612aadf442
William Hubault <william.hubault@gmail.com>
2015-08-17 17:22:30 +0200

    [fish] gl: now display changes in branch if not in master

--- fish/functions/gl.fish
+++ fish/functions/gl.fish
@@ -1,3 +1,11 @@
-function gl
-	git trlog $argv;
+function gl --description Show\ the\ changes\ made\ in\ a\ feature\ branch\ \(\'glb\'\ stands\ for:\ git\ log\ branch\)
+	set -l branch (current-git-branch)
+
+  if [ $branch != 'master' ]
+    echo "Changes made in $branch:"
+    git trlog -10000 $argv master...
+  else
+    git trlog $argv
+  end
 end
+
