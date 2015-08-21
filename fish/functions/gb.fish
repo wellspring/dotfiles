b4aea149031512c00c7e16dad254e19b20e2fa18
William Hubault <william.hubault@gmail.com>
2015-08-21 09:40:09 +0200

    [fish] Add function gb: list git branch, and display a fzf menu to switch to one.

--- fish/functions/gb.fish
+++ fish/functions/gb.fish
@@ -1,3 +1,3 @@
-function gb
-	git branch $argv; 
+function gb --description 'List branches (using fzf) and switch to one of them.'
+	git branch $argv | fzf --prompt "Git branch to switch to: " --tac --cycle --extended -i +s | xargs git checkout;
 end
