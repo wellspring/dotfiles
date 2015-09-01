50eeea007d8d65c4ca95da4b2a389827e8909e31
William Hubault <william.hubault@gmail.com>
2015-09-01 14:54:52 +0200

    [fish] Update gl function: tries to fix parent branch problem

--- fish/functions/gl.fish
+++ fish/functions/gl.fish
@@ -1,9 +1,10 @@
 function gl --description Show\ the\ changes\ made\ in\ a\ feature\ branch\ \(\'glb\'\ stands\ for:\ git\ log\ branch\)
 	set -l branch (current-git-branch)
+  set -l parent_branch (git show-branch -a 2>/dev/null | awk -F[][] '/\*/ && !/'(echo $branch | sed 's/\//\\\\&/')'/ {print $2;exit}')
 
   if [ $branch != 'master' ]
     echo "Changes made in $branch:"
-    git trlog -10000 $argv master...
+    git trlog -10000 $argv master..
   else
     git trlog $argv
   end
