9d1c9d4e85e9cf666a2c1a9a8b6007ff4c5411b9
William Hubault <william.hubault@gmail.com>
2015-09-01 16:15:17 +0200

    [fish] add function workon: create a new git branch based on a redmine task issue

--- fish/functions/workon.fish
+++ fish/functions/workon.fish
@@ -5,7 +5,7 @@ function workon --description 'Create a new branch to work on a task defined on 
   end
 
   set -l url (echo $argv | tr -d '#' | awk '!/^http/{printf "'$REDMINE_URL'issues/"} //{print $0".json"}')
-  set -l newbranch (curl -sH "X-Redmine-API-Key: $REDMINE_KEY" $url | jq -r '"'$REDMINE_USER'/"+(.issue.id|tostring)+"_\n"+.issue.subject' | sed -e '2 s/[^A-Za-z0-9 ]//g' -e 's/ \([A-Za-z0-9]\)/-\1/g' -e 's/[A-Z]/\L&/g' | tr -d '\n')
+  set -l newbranch (curl -sH "X-Redmine-API-Key: $REDMINE_KEY" $url | jq -r '"'$REDMINE_USER'/"+(.issue.id|tostring)+"-\n"+.issue.subject' | sed -e '2 s/[^A-Za-z0-9 ]//g' -e 's/ \([A-Za-z0-9]\)/-\1/g' -e 's/[A-Z]/\L&/g' | tr -d '\n')
 
   if test -z "$newbranch"
     echo "Error: task not found. Is '$argv[1]' a correct url or issue number?"
