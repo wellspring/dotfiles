4eddd9a3fddaec9a8b4235a54ac23b3f8fa6c2f8
William Hubault <william.hubault@gmail.com>
2015-09-01 14:53:12 +0200

    [fish] Update gb function: can now switch to a specified branch, or to the previous branch (gb -)

--- fish/functions/gb.fish
+++ fish/functions/gb.fish
@@ -1,3 +1,25 @@
 function gb --description 'List branches (using fzf) and switch to one of them.'
-	git branch $argv | fzf --prompt "Git branch to switch to: " --tac --cycle --extended -i +s | xargs git checkout;
+  set -l previousbranchfile (current-git-dir)/.git/previous-branch
+  set -l branch (current-git-branch)
+
+  if count $argv >/dev/null
+    if test $argv[1] = '-'
+      # (goto previous branch)
+      git checkout (cat $previousbranchfile)
+    else if test (echo $argv | head -c1) = '-'
+      # (show git branches list menu, with argument; e.g. -r for remotes branches)
+      git-branch-menu $argv
+    else
+      # (goto specified branch)
+      git checkout $argv
+    end
+  else
+    # (show local git branches list menu)
+    git-branch-menu
+  end
+
+  # Has the branch been changed? Save the previous branch name.
+  if test $branch != (current-git-branch)
+    echo $branch > $previousbranchfile
+  end
 end
