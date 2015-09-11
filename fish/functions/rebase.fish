108f14e762d49044405284eb50b6f8d53370eabe
William Hubault <william.hubault@gmail.com>
2015-09-11 19:37:50 +0200

    [fish] add empty function rebase (will be used for git)

---
+++ fish/functions/rebase.fish
@@ -0,0 +1,16 @@
+function rebase
+  # todo:
+  # if branch == master
+  # - git stash
+  # - git pull --rebase
+  # - git stash pop
+  # else
+  # - git stash
+  # - git checkout master
+  # - git pull --rebase
+  # - git checkout -
+  # - git rebase master
+  # - git stash pop
+  # end
+  # gl
+end
