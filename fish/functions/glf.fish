b249c51da669cbb1383978be5b965bd06c65fc79
William Hubault <william.hubault@gmail.com>
2015-09-11 15:24:07 +0200

    [fish] fix glf

--- fish/functions/glf.fish
+++ fish/functions/glf.fish
@@ -1,3 +1,3 @@
 function glf --description 'git log FILE (show only the commits of the file specified as an argument)'
-	git --no-pager log --pretty=format:'%Creset%C(red bold)[%ad] %C(blue bold)%h %Creset%C(magenta bold)%d %Creset%s %C(green bold)(%an)%Creset' --follow $argv;
+	git --no-pager log --pretty=format:'%Creset%C(red bold)[%ad] %C(blue bold)%h %Creset%C(magenta bold)%d %Creset%s %C(green bold)(%an)%Creset' --follow -- $argv;
 end
