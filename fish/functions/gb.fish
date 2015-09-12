0b3d643cde8cc10d3cf00003d184d6cb926081ee
William Hubault <william.hubault@gmail.com>
2015-09-12 10:59:43 +0200

    [fish] Remove handmade support of - for `gb` function, git checkout already supports parent branches

--- fish/functions/gb.fish
+++ fish/functions/gb.fish
@@ -1,12 +1,8 @@
 function gb --description 'List branches (using fzf) and switch to one of them.'
-  set -l previousbranchfile (current-git-dir)/.git/previous-branch
   set -l branch (current-git-branch)
 
   if count $argv >/dev/null
-    if test $argv[1] = '-'
-      # (goto previous branch)
-      git checkout (cat $previousbranchfile)
-    else if test (echo $argv | head -c1) = '-'
+    if test \( ! $argv[1] = '-' \) -a \( (echo $argv | head -c1) = '-' \)
       # (show git branches list menu, with argument; e.g. -r for remotes branches)
       git-branch-menu $argv
     else
@@ -16,10 +12,5 @@ function gb --description 'List branches (using fzf) and switch to one of them.'
   else
     # (show local git branches list menu)
     git-branch-menu
-  end
-
-  # Has the branch been changed? Save the previous branch name.
-  if test $branch != (current-git-branch)
-    echo $branch > $previousbranchfile
   end
 end
