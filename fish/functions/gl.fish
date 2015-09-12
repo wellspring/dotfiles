32b8b4d7d9a384d7e8dc0290b0a4aa34f494c29d
William Hubault <william.hubault@gmail.com>
2015-09-12 10:39:52 +0200

    [fish] Remove long parent branch calculation for `gl` function

--- fish/functions/gl.fish
+++ fish/functions/gl.fish
@@ -1,12 +1,14 @@
 function gl --description Show\ the\ changes\ made\ in\ a\ feature\ branch\ \(\'glb\'\ stands\ for:\ git\ log\ branch\)
-	set -l branch (current-git-branch)
-  set -l parent_branch (git show-branch -a 2>/dev/null | awk -F[][] '/\*/ && !/'(echo $branch | sed 's/\//\\\\&/')'/ {print $2;exit}')
+  set -l branch (current-git-branch)
 
   if [ $branch != 'master' ]
     echo "Changes made in $branch:"
-    git trlog -10000 $argv master.. | sed -r 's/\x1b\[38;5;198m(fixup|squash)!/\x1b[38;5;207m\1!\x1b[38;5;198m/g'
+    git trlog -10000 $argv 'master..' | sed -r 's/\x1b\[38;5;198m(fixup|squash)!/\x1b[38;5;207m\1!\x1b[38;5;198m/g'
   else
     git trlog $argv | sed -r 's/\x1b\[38;5;198m(fixup|squash)!/\x1b[38;5;207m\1!\x1b[38;5;198m/g'
   end
+
+  #@TODO: display in different color the ones already in a remote? -- should be straightforward with ack if the info is added in trlog
+  #@TODO: list changes since (parent-git-branch)                   -- also try '@{-1}..' or '@{1}..' or alternative?
 end
 
