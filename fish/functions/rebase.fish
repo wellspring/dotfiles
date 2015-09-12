1ac88de416129a357a926f7b47a5e6bed1a86450
William Hubault <william.hubault@gmail.com>
2015-09-12 10:41:09 +0200

    [fish] Add experimental `rebase` function (for git)

--- fish/functions/rebase.fish
+++ fish/functions/rebase.fish
@@ -1,16 +1,17 @@
 function rebase
-  # todo:
-  # if branch == master
-  # - git stash
-  # - git pull --rebase
-  # - git stash pop
-  # else
-  # - git stash
-  # - git checkout master
-  # - git pull --rebase
-  # - git checkout -
-  # - git rebase master
-  # - git stash pop
-  # end
-  # gl
+  if test (current-git-branch) = master
+    # On master? Then fetch remote changes on master, merge them, and re-apply the changes on top of it.
+    git stash
+    git pull --rebase
+    git stash pop
+  else
+    # On another branch? Then save what's done, fetch the changes, and re-apply the changes on top of master.
+    git stash
+    git fetch
+    git rebase origin master
+    git stash pop
+  end
+
+  # Display logs
+  gl
 end
