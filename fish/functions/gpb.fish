c4cbfc1545ddfc3089cfc63f7dcdf78541ddf8d8
William Hubault <william.hubault@gmail.com>
2015-12-06 19:09:18 +0100

    [fish] Open the browser after pushing branches

--- fish/functions/gpb.fish
+++ fish/functions/gpb.fish
@@ -1,3 +1,4 @@
 function gpb --description 'Git Push Branch'
 	git push --set-upstream origin (current-git-branch) $argv
+  firefox https://gitlab.egops.net/dev/sparkle/commits/(current-git-branch)
 end
