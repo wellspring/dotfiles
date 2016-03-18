de6b1709e67f763c1134897490539f8f335960f1
William Hubault <william.hubault@gmail.com>
2016-03-18 20:55:38 +0100

    [fish] Add functions `livecoding.url` and `livecoding.url.chat`

---
+++ fish/functions/livecoding.url.chat.fish
@@ -0,0 +1,4 @@
+function livecoding.url.chat
+	set -l livecodinguser (lower $argv[1])
+  echo "https://www.livecoding.tv/chat/$livecodinguser/"
+end
---
+++ fish/functions/livecoding.url.fish
@@ -0,0 +1,4 @@
+function livecoding.url
+	set -l livecodinguser (lower $argv[1])
+  curl -s "https://www.livecoding.tv/$livecodinguser/" | awk -F'"' '/ file:"/{print $2}'
+end
