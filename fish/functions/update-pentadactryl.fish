a1c2bb3d94cce24b01975b651bc15f3b0a1404ab
William Hubault <william.hubault@gmail.com>
2016-03-18 20:57:45 +0100

    [fish] Fix update pentadactryl to v45

--- fish/functions/update-pentadactryl.fish
+++ fish/functions/update-pentadactryl.fish
@@ -1,7 +1,7 @@
 function update-pentadactryl
 	wget http://5digits.org/nightly/pentadactyl-latest.xpi
   unzip pentadactyl-latest.xpi install.rdf
-  sed -i 's/\(em:maxVersion\)="[^"]*"/\1="42.*"/' install.rdf
+  sed -i 's/\(em:maxVersion\)="[^"]*"/\1="45.*"/' install.rdf
   zip -u pentadactyl-latest.xpi install.rdf
   rm install.rdf
   mv pentadactyl-latest{,.patched}.xpi
