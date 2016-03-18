3bf30f9399242621f33bf837bd22b615db764522
William Hubault <william.hubault@gmail.com>
2016-03-18 23:22:00 +0100

    [fish] add `matrix` and `youtube-bg` animated background functions

---
+++ fish/functions/matrix.fish
@@ -0,0 +1,3 @@
+function matrix
+	xwinwrap -ov -fs -- /usr/lib/xscreensaver/glmatrix -root -window-id WID
+end
---
+++ fish/functions/youtube-bg.fish
@@ -0,0 +1,3 @@
+function youtube-bg
+	xwinwrap -ov -fs -- mpv -wid WID -panscan 1 -loop $argv
+end
