4bd7965db5a6c4ebfa86b5b86dcb7337f76b82ac
William Hubault <william.hubault@gmail.com>
2016-05-10 07:18:37 +0200

    [fish] make `p` execute `mplayer` instead of `package`

--- fish/functions/p.fish
+++ fish/functions/p.fish
@@ -1,3 +1,7 @@
-function p --description 'alias for package'
-	package $argv;
+function p --description 'mplayer (with remote control)'
+	set -l sock /tmp/mplayer-ctrl.sock
+  rm -f $sock
+  mkfifo $sock
+	mplayer -really-quiet -input file=$sock -subcp utf8 $argv
+  #echo pause > $sock
 end
