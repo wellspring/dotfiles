4ddc51cd38160deb696cbf1e0a9198eaf2beeb75
William Hubault <william.hubault@gmail.com>
2016-08-31 21:16:52 +0200

    [fish] Update `livecoding.url`

--- fish/functions/livecoding.url.fish
+++ fish/functions/livecoding.url.fish
@@ -1,4 +1,4 @@
 function livecoding.url
-  set -l stream (echo $argv[1] | sed 's_^https\?://\(www\.\)\?livecoding\.tv/__')
-  curl -s "https://www.livecoding.tv/$stream" | awk -F'"' '/ file:"/{print $2}' | tail -1
+  set -l stream (echo $argv[1] | awk '!/^http/{print $0"/"}' | sed 's_^https\?://\(www\.\)\?livecoding\.tv/__')
+  curl -s "https://www.livecoding.tv/$stream" | awk -F'"' '/ file:"/{print $2}' | grep rtmp
 end
