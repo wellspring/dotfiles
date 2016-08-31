80062ec3d76ae5d053f60e848e7b150aa192e134
William Hubault <william.hubault@gmail.com>
2016-08-31 21:14:39 +0200

    [fish+i3] Update the `demo` function to spawn a terminal window instead

--- fish/functions/demo.fish
+++ fish/functions/demo.fish
@@ -1,3 +1,16 @@
 function demo --description 'Run a shell using the demo user'
-	sudo su demo
+  # Start transparency manager & screenkey (to see key pressed)
+  compton --config ~/.compton.conf &
+  screenkey --opacity 0.5
+
+  # Create a terminal (X)
+	urxvt -title 'demo::tty' \
+        -g 80x25 \
+        -fn "xft:Anonymous Pro for Powerline:pixelsize=18,style=regular" \
+        -fb "xft:Anonymous Pro for Powerline:pixelsize=18,style=Bold" \
+        -e sudo -Hiu demo
+
+  # Cleanup.
+  killall compton
+  killall screenkey
 end
