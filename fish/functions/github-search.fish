e8beaf22d34a6c6fe224d15d912ce86917823ad8
William Hubault <william.hubault@gmail.com>
2016-08-31 21:17:47 +0200

    [fish] Update `github-search` (more results + log query)

--- fish/functions/github-search.fish
+++ fish/functions/github-search.fish
@@ -1,3 +1,5 @@
 function github-search
-	curl -sG https://api.github.com/search/repositories -d "q=$argv" | jq -r '.items[] | "%x1b[0m- %x1b[32m"+.name+"%x1b[0m: %x1b[33m"+.description+" %x1b[0m--%x1b[34m "+.html_url'| sed 's/%x1b/\x1b/g' | tee "/tmp/last-github-search-$USER" | head -15
+  set -l query (echo $argv | tr ' ' '+')
+  echo $query > /tmp/last-github-search-$USER.query
+	curl -sG https://api.github.com/search/repositories'?page=1&per_page=100' -d "q=$query" | jq -r '.items[] | "%x1b[0m- %x1b[32m"+.name+"%x1b[0m: %x1b[33m"+.description+" %x1b[0m--%x1b[34m "+.html_url'| sed 's/%x1b/\x1b/g' | tee "/tmp/last-github-search-$USER" | less
 end
