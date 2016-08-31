4e49d228619623cfa2f9ceaa9210affe34cf8734
William Hubault <william.hubault@gmail.com>
2016-08-31 21:15:39 +0200

    [fish] make `ssa` only keep the key for 30min

--- fish/functions/ssa.fish
+++ fish/functions/ssa.fish
@@ -1,4 +1,4 @@
-function ssa --description 'start ssh-agent and ask the key using ssh-add'
-	ssh-agent -c | sed 's/setenv/set -Ux/' | fish
+function ssa --description 'start ssh-agent and ask the key using ssh-add (30min)'
+	ssh-agent -c -t 1800 | sed 's/setenv/set -Ux/' | fish
   ssh-add
 end
